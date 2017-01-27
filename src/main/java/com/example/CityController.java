1685f1c7ca6c7e0267e60984b6e04bc49189b93e
making <makingx@gmail.com>
2017-01-27 16:24:04 +0900

    1.4 ize

--- src/main/java/com/example/CityController.java
+++ src/main/java/com/example/CityController.java
@@ -2,9 +2,9 @@ package com.example;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,12 +15,12 @@ public class CityController {
 		this.cityRepository = cityRepository;
 	}
 
-	@RequestMapping(value = "/cities", method = RequestMethod.GET)
+	@GetMapping("/cities")
 	List<City> getCities() {
 		return cityRepository.findAll();
 	}
 
-	@RequestMapping(value = "/cities", method = RequestMethod.POST)
+	@PostMapping("/cities")
 	City postCities(@RequestBody City city) {
 		return cityRepository.save(city);
 	}
